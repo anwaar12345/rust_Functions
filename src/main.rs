fa90a82ba0ff119f66a46bee3ae93969c9153c29
anwaar12345 <syedanwar016@gmail.com>
2019-10-06 03:00:39 +0500

    function with on return value and to paramenters

--- src/main.rs
+++ src/main.rs
@@ -1,129 +1,32 @@
-/*/*
-fn main(){
-let v = ret(-4);
-if v > 0{
-println!("positive{}",v);    
-}else{
-    println!("number is negative {}",v);
+/* fn main(){
+let per :u64 = per(80,56);
+if per > 33 {
+println!("passed with \n {} %",per);    
+}
 }
 
-second(1,1.4,'A',true);
-
-let per =result();
-
-if per >= 80{
-
-println!("You got {} % \n Grade is A+",per);
-
-}else if per < 80 && per >= 70{
-
-println!("You got {} % \n grade is A",per);
+fn per(maths:u64,computer:u64) -> u64{
+    let total: u64 = 200;
     
-}else if per < 70 && per >= 60 {
-
-println!("You got {} % \n grade is B",per);
-    
-}else if per < 60 && per >= 50 {
-
-println!("You got {} % \n grade is C",per);
-    
-}else if per < 50 && per >= 40 {
-
-println!("You got {} % \n grade is D",per);
-    
-}else{
-    println!("failed");
+    let marksobtain =maths + computer;
+    let per = marksobtain * 100 / total;
+    per 
 }
 
-
-}
-
-//Assignment second question
-fn second(first :u64, second :f64, third: char, fourth: bool){
-    println!("The Value Of int  is :{} \nthe value of float is:{} \n the value of char is :{} \n the value of Bool is {}",first,second,third,fourth);
-
-}
-
-//Assignment 1st Question
-fn ret( number:i64) -> i64{
-let n = number;
-n
-
-}
-//Assignment 4th Question
-
-fn result() -> u64{
-    let maths:u64 = 100;
-    let computer:u64 = 100;
-    let mathmarks =70;
-    let computermarks = 94;
-println!("Each subject has Total of following  marks \n Maths : {} \n computer : {}",maths,computer);
-    let _total: u64 = maths + computer;
-    println!("Total Marks are = {}",_total);
-    let _mytotal: u64 = mathmarks + computermarks;
-    
-    let per = _mytotal * 100 / _total; 
-    per
-}
-*/
-fn main(){
-   let v = number(4);
-let tup = (v);
-
-println!("The Value Of number is: {}\n tup is :{}",v,tup * tup);
-}
-
-fn number(num:u64) -> u64{
-num
-} 
 */
 
-//second Practice for RUST Functions
 
 fn main(){
-    println!("message from \n");
-    cal();
-    sandexp();
-    
-    //arguments and parameters
-    
-    argpara(4);
-    
-    //function with returning value
-let n = value(6);
-println!("n:\n {}",n);
-
+let per :f64 = per(80,56);
+if per > 33.0 {
+println!("passed with \n {} %",per);    
 }
-//main function Above 
-//simple function Definition
-fn cal(){
-    println!("i am from called function");
 }
 
-// Statement And Expression Definition
-
-fn sandexp(){
-
-//statement x
-let x = 9;
-println!("\n statement x:\n{}",x);
-
-//Expression num
-let num={
-    let o = 9;
-    o + 1
+fn per(maths:u64,computer:u64) -> f64{
+    let total: u64 = 200;
     
-};
-println!("\n Value from Expression is :\n{}",num);
-}
-//Arguments and Parameters
-
-fn argpara(x:u64){
-println!("The value Got from Parameter is:\n{}",x);
-}
-
-//functions with returning values
-
-fn value(y: u64) -> u64{
-    y
+    let marksobtain =maths + computer;
+    let per: f64 = (marksobtain * 100 / total) as f64;
+    per 
 }
