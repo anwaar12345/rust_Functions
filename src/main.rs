79fc3188ee3bf3c72171a2d03a662fc9466e311f
anwaar12345 <syedanwar016@gmail.com>
2019-10-06 03:08:16 +0500

    Tuples returning to main function

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,11 @@
-/* fn main(){
-let per :u64 = per(80,56);
-if per > 33 {
-println!("passed with \n {} %",per);    
-}
-}
-
-fn per(maths:u64,computer:u64) -> u64{
-    let total: u64 = 200;
-    
-    let marksobtain =maths + computer;
-    let per = marksobtain * 100 / total;
-    per 
-}
-
-*/
-
-
 fn main(){
-let per :f64 = per(80,56);
-if per > 33.0 {
-println!("passed with \n {} %",per);    
-}
+let (num,sqn) = tupr(4);
+println!("num is : \n {}",num);
+println!("\n square is : \n {}",sqn);
 }
 
-fn per(maths:u64,computer:u64) -> f64{
-    let total: u64 = 200;
-    
-    let marksobtain =maths + computer;
-    let per: f64 = (marksobtain * 100 / total) as f64;
-    per 
+fn tupr(num: u64) -> (u64,u64){
+
+    let sqn = num *  num;
+    (num,sqn)
 }
